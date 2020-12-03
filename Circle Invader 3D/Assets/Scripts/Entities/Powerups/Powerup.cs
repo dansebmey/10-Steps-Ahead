b4c8cacf5108bdc2367fae2428f47774a0604021
Daniel Meyer <dansebmey@gmail.com>
2020-12-03 13:47:53 +0100

    Some code rearranging

--- Circle Invader 3D/Assets/Scripts/Entities/Powerups/Powerup.cs
+++ Circle Invader 3D/Assets/Scripts/Entities/Powerups/Powerup.cs
@@ -10,15 +10,43 @@ public abstract class Powerup : OrbitingObject
     public int initDuration;
     [HideInInspector] public int remainingDuration;
     [HideInInspector] public bool isUpgraded;
+    
+    
+    private float _offset;
+    private float _scaleFactor;
 
+    
     protected override void Start()
     {
         base.Start();
         Gm.PowerupManager.RegisterPowerup(this);
+        _scaleFactor = 1.0f / transform.localScale.x;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        _offset += 0.01f;
+        Vector3 pos = transform.position;
+        pos = new Vector3(
+            pos.x,
+            pos.y + _scaleFactor * 0.33f * Mathf.Sin(_offset),
+            pos.z);
+        transform.position = pos;
+
+        transform.Rotate(new Vector3(0, 0.5f, 0));
+        
+        // TODO: Replace this with an Animator
     }
 
     public virtual void OnPickup()
     {
-        Debug.Log(name + " was picked up!");
+        if (Gm.player.AddToInventory(this))
+        {
+            Debug.Log(name + " was picked up!");
+            transform.parent = Gm.player.transform;
+            // Destroy(gameObject);
+        }
     }
 }
